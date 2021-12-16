1cfd085a796b21041f7c01f73e0bc707bdcc7710
FUJII Hiroshi <fujii@acutus.co.jp>
2021-12-17 00:01:24 +0900

    delete body-parser

--- sample-login/app.js
+++ sample-login/app.js
@@ -2,7 +2,6 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser')
 var logger = require('morgan');
 
 var authRouter = require('./routes/auth');
@@ -17,7 +16,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', authRouter);
