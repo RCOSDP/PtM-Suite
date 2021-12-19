419cc4ab8be731e712619ed1c515dc4f15b4d259
FUJII Hiroshi <fujii@acutus.co.jp>
2021-12-19 21:51:55 +0900

    get token from origin+/login

--- sample-login/routes/auth.js
+++ sample-login/routes/auth.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const axios = require('axios');
+const https = require('https');
+const setCookie = require('set-cookie-parser');
 
 const router = express.Router();
 
@@ -9,10 +12,22 @@ router.get('/dialog/start', function(req, res) {
   res.render('start', data);
 });
 
-router.post('/dialog/start', function(req, res) {
+router.post('/dialog/start', async function(req, res) {
   console.log(req.body);
-  res.cookie('session_cookie', req.body.user_id, {
-    path: "/",
+  const httpsAgent = new https.Agent({ rejectUnauthorized: false });
+  const login = await axios.post(req.headers.origin + '/login',{
+    user_id: req.body.user_id,
+    password: process.env.POLLY_PASSWORD,
+  },{
+    httpsAgent,
+  });
+
+  const cookies = setCookie.parse(login, {
+    map: true,
+  });
+  console.log(cookies['session_cookie']);
+
+  res.cookie('session_cookie', cookies['session_cookie'], {
     secure: false,
     httpOnly: false,
   });
