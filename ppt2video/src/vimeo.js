fb6745ffc4c7cf9b35f1bb4510329cf156b162b4
ties-makimura <makimura@cccties.org>
2021-12-24 11:23:29 +0900

    fix: JSONファイルにはまとめて書きこむよう修正
    #27 の対応
    Vimeo登録後に受け取るURLを取得するたび，JSONファイルに読み書きを行っていた処理を止めて，全てのURLを取得し終えてからまとめて書きこむように変更．

--- ppt2video/src/vimeo.js
+++ ppt2video/src/vimeo.js
@@ -15,19 +15,23 @@ var mp4Files = outputList.filter(val => val.match(/.mp4$/g));
 
 var client = new Vimeo(CLIENT_ID, CLIENT_SECRET, ACCESS_TOKEN);
 
-function jsonReplace (jsonFile,fileName,url){
+function jsonReplace (jsonFile,vimeolist){
   fs.readFile(jsonFile, 'utf8', function (err,data) {
     if (err) {
       return console.log(err);
     }
-    var result = data.replace('"file": "'+fileName+'"','"url": "'+url+'"');
-    fs.writeFile(jsonFile, result, 'utf8', function (err) {
+    for (const key in vimeolist) {
+        data = data.replace('"file": "'+key+'"','"url": "'+vimeolist[key]+'"');
+    }
+    fs.writeFile(jsonFile, data, 'utf8', function (err) {
        if (err) return console.log(err);
     });
   });
 }
 
+const vimeolist = {};
 function uploadVimeo(client,jsonFile,mp4Files) {
+  let cnt = mp4Files.length;
   mp4Files.forEach(val => {
     let fileName = path.basename(val);
     client.upload(
@@ -36,8 +40,13 @@ function uploadVimeo(client,jsonFile,mp4Files) {
         'name': fileName
       },
       function (uri) {
-        console.log('File upload',fileName,'completed. Your Vimeo URI is:', uri)
-        jsonReplace(jsonFile,fileName,uri.replace('/videos/','https://vimeo.com/'))
+        console.log('countdown:', cnt,' File upload:',fileName,' completed. Your Vimeo URI is:', uri)
+        vimeolist[fileName] = uri.replace('/videos/','https://vimeo.com/')
+        if(cnt == 1){
+          console.log('vimeolist:', vimeolist)
+          jsonReplace(jsonFile,vimeolist)
+        }
+        cnt--;
         client.request({path: '/me/projects/' + PROJECT_ID + uri, method: 'PUT'}, function (error, body, status_code, headers) {console.log('video to a folder',body);})
         fs.unlink(val, (err) => { if (err) throw err; console.log('deleted',val)})
       },
