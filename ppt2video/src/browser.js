8f1351a48e8b55a944c780b6e2864b7421534c6b
FUJII Hiroshi <fujii@acutus.co.jp>
2023-03-24 16:04:15 +0900

    implement listSlideImages

--- ppt2video/src/browser.js
+++ ppt2video/src/browser.js
@@ -50,3 +50,51 @@ async function init() {
 function createImportJson() {
   return convert(this, this.slides, this.sections);
 }
+
+//
+// preparation
+//
+
+export async function listSlideImages(path) {
+  const slides = {};
+  const re = /(?<page>\d+).(png|PNG|jpg|JPG)/;
+  const dir = await openDir(path);
+  for await (const dirent of dir.entries()) {
+    const match = dirent[0].match(re);
+    if (match != null) {
+      slides[match.groups.page] = match.input;
+    }
+  }
+  return slides;
+}
+
+function prepare(filepath, sections, vsuffix, asuffix) {
+  const imageDir = path.join(filepath.dir, filepath.name);
+  const imageFiles = listSlideImages(imageDir);
+  let tnum = 1;
+  let snum = 1;
+
+  sections.forEach(section => {
+    section.topics.forEach(topic => {
+      topic.file = `${filepath.name}_${tnum}.${vsuffix}`;
+      topic.outputFilename = path.join(outputDir, topic.file);
+      topic.slides.forEach(slide => {
+        slide.audioFilename = path.join(tempDir, `${filepath.name}_${tnum}_${snum}.${asuffix}`);
+        slide.videoFilename = path.join(tempDir, `${filepath.name}_${tnum}_${snum}.${vsuffix}`);
+        const imageFilename = imageFiles[snum];
+        if (typeof imageFilename !== 'undefined') {
+          slide.imageFilename = path.join(imageDir, imageFilename);
+        } else {
+          throw "can't find slide image file for slide number " + snum;
+        }
+        snum = snum + 1;
+      });
+      if (topic.slides.length > 1) {
+        topic.listFilename = path.join(tempDir, `${filepath.name}_${tnum}.list`);
+        const list = topic.slides.map(slide => 'file ' + path.relative(tempDir, slide.videoFilename));
+        fs.writeFileSync(topic.listFilename, list.join('\n'));
+      }
+      tnum = tnum + 1;
+    });
+  });
+}
